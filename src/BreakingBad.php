d12be7c778e3dcaa3e071003d4b02ddd442c186a
Naveed <08es34@gmail.com>
2019-12-19 13:31:42 +0500

    basic stuff

---
+++ src/BreakingBad.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Naveed\Scaff;
+
+class BreakingBad
+{
+    public static function sayMyName()
+    {
+        echo "Heisenberg!\n";
+        echo "You're goddamn right!!";
+    }
+}
