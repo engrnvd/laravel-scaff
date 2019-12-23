c159e947142a749510c824e8309076444dcc7240
Naveed <08es34@gmail.com>
2019-12-23 21:14:41 +0500

    help block color

--- src/views/vue-shared/apm-form-element.blade.php
+++ src/views/vue-shared/apm-form-element.blade.php
@@ -2,7 +2,7 @@
     Vue.component('apm-form-element', {
         template: '<div class="form-group" :class="{\'has-error\': errors}">' +
             '        <slot></slot>' +
-            '        <span class="help-block text-danger" v-if="errors" v-for="msg in errors">@{{msg}}</span>' +
+            '        <span class="red--text text--darken-4" v-if="errors" v-for="msg in errors">@{{msg}}</span>' +
             '    </div>',
         props: {
             model: Object,
