2615c3ad3d372e24e391e1ad4377d314ea869a4d
Naveed <08es34@gmail.com>
2020-01-02 22:41:52 +0500

    scaff for create

--- src/templates/create.blade.php
+++ src/templates/create.blade.php
@@ -4,46 +4,43 @@
 ?>
 
 <template>
-    <b-modal id="createUserModal"
-             ref="createUserModal"
-             title="Add New User"
+    <b-modal id="create{{$table->studly(true)}}Modal"
+             ref="create{{$table->studly(true)}}Modal"
+             title="Add New {{$table->title(true)}}"
              hide-footer
              modal-class="modal-right">
-        <apm-form action="/users" v-model="form">
-            <apm-form-element field="first_name" label="First Name" :model="form">
-                <b-form-input v-model="form.first_name"/>
+        <apm-form action="/{{$table->slug()}}" v-model="form">
+@foreach($table->fields as $field)
+            <apm-form-element field="{{$field->name}}" label="{{$field->title()}}" :model="form">
+@if($field->type === 'enum' && count($field->enumValues) < 3)
+                <b-form-radio-group class="pt-2" :options="['{{join("', '", $field->enumValues)}}']" v-model="form.{{$field->name}}"/>
+@elseif($field->type === 'text')
+                <b-textarea v-flex-height v-model="form.{{$field->name}}" :rows="2" :max-rows="2"/>
+@elseif($field->type === 'enum')
+                <v-select :options="['{{join("', '", $field->enumValues)}}']" v-model="form.{{$field->name}}"/>
+@elseif($field->type === 'boolean')
+                <switches v-model="form.{{$field->name}}" theme="custom" color="primary"></switches>
+@else
+                <b-form-input v-model="form.{{$field->name}}"/>
+@endif
             </apm-form-element>
-            <apm-form-element field="last_name" label="Last Name" :model="form">
-                <b-form-input v-model="form.last_name"/>
-            </apm-form-element>
-            <apm-form-element field="email" label="Email" :model="form">
-                <b-form-input type="email" v-model="form.email"/>
-            </apm-form-element>
-            <apm-form-element label="Gender" field="gender" :model="form">
-                <v-select :options="['Male', 'Female']" v-model="form.gender"/>
-            </apm-form-element>
-            <apm-form-element label="Description" field="description" :model="form">
-                <b-textarea v-flex-height v-model="form.description" :rows="2" :max-rows="2"/>
-            </apm-form-element>
-            <apm-form-element label="Status" field="status" :model="form">
-                <b-form-radio-group stacked class="pt-2" :options="['Active', 'Inactive']" v-model="form.status"/>
-            </apm-form-element>
-            <b-button variant="outline-secondary" @click="$refs.createUserModal.hide()" class="mr-2">Cancel</b-button>
-            <b-button variant="primary" type="submit" class="mr-1">Submit</b-button>
+@endforeach
+            <b-button variant="outline-secondary" @click="$refs.create{{$table->studly(true)}}Modal.hide()" class="mr-2">Cancel</b-button>
+            <b-button variant="primary" type="submit" class="mr-1">Save</b-button>
         </apm-form>
     </b-modal>
 </template>
 
 <script>
     export default {
-        name: "CreateUserModal",
+        name: "Create{{$table->studly(true)}}Modal",
         props: {},
         data() {
             return {
                 form: {
-                    title: '',
-                    description: '',
-                    status: 'Active'
+@foreach($table->fields as $field)
+                    {{$field->name}}: '{{ $field->default ?? '' }}',
+@endforeach
                 }
             }
         },
